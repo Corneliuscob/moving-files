3aad3e8752dc48ab3fbc4cc4d866bf11f89ed0e1
Ngai To <ngai_to_lo@hotmail.com>
2019-01-10 23:40:48 -0800

    added some files to learn bash

---
+++ hello.sh
@@ -0,0 +1,24 @@
+ #!/bin/bash       
+
+
+### reference http://tldp.org/HOWTO/Bash-Prog-Intro-HOWTO-3.html    
+# echo prints stuf
+echo Hello World 
+## the > operator writes stuff to files . eg ls -l >ls.txt will writ ethe output of ls to a txt 
+
+#### Grep looks for text in something 
+### grep da * looks through  directory to find text  with da
+
+
+## pipes use the output and push them to the input of the next program
+# sed replaces all vowels with u
+ls  | sed -e "s/[aeio]/u/g"  
+
+###Probably, this is a more difficult way to do ls -l *.txt, but it is here for illustrating pipes, not for solving such listing dilema.
+ls -l | grep "\.txt$"
+#Here, the output of the program ls -l is sent to the grep program, which, in turn, will print lines which match the regex "\.txt$".
+
+
+### variables 
+STR="this is inside a variable!"
+echo $STR    
